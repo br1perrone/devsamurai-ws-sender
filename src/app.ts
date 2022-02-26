bb663c0014585e6d76063aac9f0f0f251306bec6
br1perrone <brunoperrone07@gmail.com>
2022-02-26 18:43:41 -0300

    funções status, send e list_menu da API

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+import 'dotenv/config'
+import express, {Request, Response} from 'express'
+
+import Sender from "./sender"
+
+const sender = new Sender()
+
+const app = express()
+
+const status = (req :Request, res :Response) => {
+    return res.send({
+        qr_code: sender.qrCode,
+        connected: sender.isConnected,
+    })
+}
+const send = async (req :Request, res :Response) => {
+    const {number, message} = req.body
+    try {
+        await sender.sendText(number, message)
+        return res.status(200).json()
+    } catch (error) {
+        // https://youtu.be/uCoSzw9L0SQ?t=3159
+        console.error(error)
+        return res.status(500).json({status: "error", message: error})
+    }
+}
+const list_menu = async (req :Request, res :Response) => {
+    const {number, title, subtitle, description, button, list } = req.body
+    try {
+        const ret = await sender.sendListMenu(number, title, subtitle, description, button, list)
+        return res.status(200).json({data: ret})
+    } catch (error) {
+        // https://youtu.be/uCoSzw9L0SQ?t=3159
+        console.error(error)
+        return res.status(500).json({status: "error", message: error})
+    }
+}
+
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// ROUTES
+app.get('/status', status)
+app.get('/send', send)
+app.post('/send', send)
+app.post('/list_menu', list_menu)
+
+app.listen(PORT, ()=> console.log('🤖⚡ server started, is running at PORT #', PORT))
--- src/sender.ts
+++ src/sender.ts
@@ -1,10 +1,20 @@
-import { start } from 'repl'
+import { parsePhoneNumber, isValidPhoneNumber } from 'libphonenumber-js'
 import { create, Whatsapp, Message, SocketState } from 'venom-bot'
 
 export type QRCode = {
     baseQR :string
     asciiQR :string
     attempts :number
+}
+
+export type MenuItem = {
+    title :string
+    description :string
+}
+
+export type ListMenu = {
+    title: string
+    rows: MenuItem[]
 }
 
 class Sender {
@@ -16,39 +26,72 @@ class Sender {
         this.initialize()
     }
 
+    get qrCode() :QRCode {
+        return this.qr
+    }
+
+    get isConnected() :boolean {
+        return this.connected
+    }    
+
     private initialize() {
         const qr = (baseQR :string, asciiQR :string, attempts :number) => {
             this.qr = {baseQR, asciiQR, attempts}
         }
 
-        const status = (statusSession :string) => {
+        const statusFind = (statusSession :string, status :string) => {
             this.connected = [
                 'isLogged',
                 'qrReadSuccess',
                 'chatsAvailable',
             ].includes(statusSession)
+            console.log('Status', status)
         }
 
         const start = (client :Whatsapp) => {
             this.client = client
+
+            client.onStateChange((state) => {
+                this.connected = state === SocketState.CONNECTED
+            })
         }
 
-        create('ws-sender', qr, status)
+        create('ws-sender', qr, statusFind)
             .then((client) => start(client))
             .catch((error) => console.error(error))
     }
 
-    async sendText(to :string, body :string) {
+    private chatId(to :string) {
         // (12) 982041640
         // 5512982041640@c.us
-        
-        let chatId :string = to
+
+        if (! isValidPhoneNumber(to, 'BR')) {
+            throw new Error('Invalid phone number')
+        }
+
+        let chatId :string = parsePhoneNumber(to, 'BR')
+            .format('E.164')
+            .replace('+', '')
+
         chatId = chatId.includes('@c.us')
             ? chatId
             : `${chatId}@c.us`
-        chatId = chatId
-            ?.replace('+', '')
-        this.client.sendText(chatId, body)
+        
+        return chatId
+    }
+
+    async sendText(to :string, body :string) {
+        await this.client.sendText(this.chatId(to), body)
+    }
+
+    async sendListMenu(to :string, title :string, subTitle :string, description :string, button :string, list :ListMenu) {
+        await this.client.sendListMenu(this.chatId(to), title, subTitle, description, button, list as any)
+            .then((result) => {
+                return result;
+            })
+            .catch((error) => {
+                throw new Error(`Error when sending: ${error}`);
+            });
     }
 }
 
